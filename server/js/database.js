a482faf71b315c066827da438090a548e9c774d2
sampersand <westerhack@gmail.com>
2018-12-20 14:55:43 -0800

    communications work

--- server/js/database.js
+++ server/js/database.js
@@ -12,15 +12,41 @@ function connect_to_database (database, callback) {
 	})
 }
 
+function filter_fields(json) {
+	var result = {};
+	for (var i = 1; i < arguments.length; i++) {
+		var field = arguments[i];
+
+		if (!(result[field] = json[field]))
+			return [false, `missing field '${field}'`];
+	}
+	return [true, result]
+}
+
+function login_user(user, cb_ok, cb_err) {
+    [ok, filtered] = filter_fields(user, 'username', 'password');
+    if(!ok)
+    	return cb_err(filtered);
+    cb_ok('<logged in but not actually cause i havent done that yet>');
+}
+
+function register_user(user, cb_ok, cb_err) {
+    [ok, filtered] = filter_fields(user, 'username', 'password', 'email');
+    if(!ok)
+    	return cb_err(filtered);
+    add_user(filtered, cb_ok);
+}
+
+
 function add_user(user, callback) {
 	connect_to_database('users', db => {
 		// TODO: figure otu a way to disallow adding with the same username / email
-		db.db('users').collection('users').insertOne(user, (err, res) => {
+		db.db('users').collection('users').insertOne(user, (err, result) => {
 			if (err)
 				throw err;
 			console.log(`Added user: ${user.username} (${JSON.stringify(user)})`);
 			db.close();
-			callback(res);
+			callback(result);
 		})
 	})
 }
@@ -53,7 +79,6 @@ function del_user(username, callback) {
 
 
 module.exports = {
-	add_user: add_user,
-	get_user: get_user,
-	del_user: del_user
+	login_user: login_user,
+	register_user: register_user
 }
--- server/js/redis.js
+++ server/js/redis.js
@@ -1,18 +1,20 @@
 process.chdir('..'); // so we're in the base
 
-var express = require('express');
-var redis   = require("redis");
-var session = require('express-session');
-var redisStore = require('connect-redis')(session);
-var bodyParser = require('body-parser');
-var client  = redis.createClient();
+const express = require('express'),
+      redis = require("redis"),
+      session = require('express-session'),
+      redisStore = require('connect-redis')(session),
+      bodyParser = require('body-parser'),
+      database = require('./database.js');
+
+var client = redis.createClient();
 var app = express();
 
 app.set('views', 'webpages');
 app.engine('html', require('ejs').renderFile);
 
 app.use(session({
-    secret: 'ssshhhhh',
+    secret: 'lol this is a secwet',
     // create new redis store.
     store: new redisStore({ host: 'localhost', port: 6379, client: client, ttl: 260}),
     saveUninitialized: false,
@@ -22,51 +24,54 @@ app.use(session({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/register.html', (req, res) => {
-	res.render('register.html');
+app.get('/register.html', (request, response) => {
+	response.render('register.html');
 });
 
-app.get('/', (req, res) => {
+app.get('/', (request, response) => {
     // create new session object.
-    console.log(req.session.key);
-    if(req.session.key) {
+
+    if(request.session.key) {
         // if email key is sent redirect.
-        res.render('index.html');
-        // res.redirect('/admin');
+        response.render('index.html');
+        // response.redirect('/admin');
     } else {
         // else go to home page.
-        res.render('index.html');
+        response.render('index.html');
     }
 });
 
-app.post('/__add_status', (req, res) => {
+app.post('/__add_status', (request, response) => {
     // when user login set the key to redis.
-    console.log("" + JSON.stringify(req.body));
-    req.session.key=req.body.email;
-    res.json({success: true, msg: req.body['msg']});
+    console.log("" + JSON.stringify(request.body));
+    request.session.key=request.body.email;
+    response.json({success: true, msg: request.body['msg']});
 });
 
-app.post('/register', (req,res) =>{
+app.post('/register', (request,response) =>{
     // when user login set the key to redis.
-    console.log("" + JSON.stringify(req.body))
-    req.session.key=req.body.email;
-    res.end('done');
+    database.register_user(request.body, _ok => {
+        response.json({ success: true, });
+    }, err => {
+        response.json({ success: false, msg: err })
+    })
 });
 
-
-app.post('/login', (req,res) => {
-    // when user login set the key to redis.
-    console.log("" + JSON.stringify(req.body))
-    req.session.key=req.body.username;
-    res.end('done');
+app.post('/login', (request, response) => {
+    database.login_user(request.body, _ok => {
+        request.session.key = request.body.username;
+        response.json({ success: true, });
+    }, err => {
+        response.json({ success: false, msg: err })
+    });
 });
 
-app.get('/logout',(req,res) => {
-    req.session.destroy(err => {
+app.get('/logout',(request,response) => {
+    request.session.destroy(err => {
         if(err){
             console.log(err);
         } else {
-            res.redirect('/');
+            response.redirect('/');
         }
     });
 });
